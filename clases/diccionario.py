c035e6dbcda589a8bd417976ebf0d369deb57577
Sirley Castillo <sirley..castillo89@gmail.com>
2019-07-18 00:15:34 +0200

    como acceder a la info de las pelis

--- clases/diccionario.py
+++ clases/diccionario.py
@@ -1,1 +1,23 @@
 ### stackoverflow = solucionador de problemas
+# OMDb api # para peliculas contrsenha: 
+
+import requests
+key = "4e9fcd9d"
+url= "http://www.omdbapi.com/?apikey="
+titulo = "fast & furius"
+busqueda = url + key+ "&t=" + titulo
+
+resultado = requests.get(busqueda)
+dicc= resultado.json()  # guardamos en una variable para usar despues
+print(dicc )
+print(" el director es: ", dicc["Director"])
+
+def p (titpel):
+    busqueda = url + key+ "&t=" + titulo
+    resultado = requests.get(busqueda)
+    dicc= resultado.json()
+    return dicc["Director"]
+print(p("MOANA"))
+
+
+ 
